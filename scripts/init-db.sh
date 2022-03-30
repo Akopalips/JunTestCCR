04eae5a0811acdd8cec0a95fc9835fa8cb87ffa0
Andrey Rogov <andrey.rogov@amtelsoft.ru>
2022-03-30 17:44:31 +0300

    Добавлены docker-специфичные штуки

---
+++ scripts/init-db.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE TABLE news (
+        name text not null,
+        type_id int not null,
+        about_short text,
+        about_full text,
+        id bigserial primary key
+    );
+    CREATE TABLE news_type (
+        name text not null,
+        color text,
+        id bigserial primary key
+    );
+EOSQL
