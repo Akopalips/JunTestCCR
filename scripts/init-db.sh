ce9a2af5eac9c92984b43402e3e62659508795d4
Akopalips <50484098+Akopalips@users.noreply.github.com>
2022-04-02 21:41:29 +0700

    Now news.type_id refers to news_type.id.

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -5,7 +5,7 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE TABLE news (
         name text not null,
-        type_id int not null,
+        type_id bigint not null references news_type(id) on delete restrict on update cascade,
         about_short text,
         about_full text,
         id bigserial primary key
